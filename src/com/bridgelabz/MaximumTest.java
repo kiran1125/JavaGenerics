09849790c559cef17e8743f6819e1c6a11798046
kiran1125 <makamkiran95@gmail.com>
2021-11-04 20:45:58 +0530

    Refactored: compare to methods for finding maximum number

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -7,7 +7,7 @@ package com.bridgelabz;
 
 public class MaximumTest {
    //class level variables
-   public int x,y,z;
+   public Integer x,y,z;
 
    /**
     *passing three integers as parameters0 to get maximum number
@@ -16,16 +16,16 @@ public class MaximumTest {
     * @param z
     * @return maximum
     */
-   public int maximumOfIntegers(int x,int y, int z){
+   public int maximumOfIntegers(Integer x,Integer y, Integer z){
       this.x = x;
       this.y = y;
       this.z = z;
       int maximum=0;
-      if(x>y && x>z)
+      if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
          maximum = x;
-      if (y>x && y>z)
+      if (y.compareTo(x) > 0 && y.compareTo(z) > 0)
          maximum = y;
-      if (z>x && z>y)
+      if (z.compareTo(x) > 0 && z.compareTo(y) > 0)
          maximum = z;
       return maximum;
    }
