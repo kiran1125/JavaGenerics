ae6f28870bd856c88562a922899a45c1f18f51bf
kiran1125 <makamkiran95@gmail.com>
2021-11-04 21:38:18 +0530

    Refactored: Generic type T to get the maximum from given type

--- src/com/bridgelabz/PrintMaximumValue.java
+++ src/com/bridgelabz/PrintMaximumValue.java
@@ -2,6 +2,6 @@ package com.bridgelabz;
 public class PrintMaximumValue {
     public static void main(String[] args) {
         MaximumTest maximumTest = new MaximumTest();
-        System.out.println("Maximum of the three integers is : " +maximumTest.maximumOfStrings("apple","waterMelon","Banana"));
+        System.out.println("Maximum of the three integers is : " +maximumTest.maximumOfObjects(2.5d,3.4d,4d));
     }
 }
