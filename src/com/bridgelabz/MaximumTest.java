37157a1f4f9f3361d1a3a72ac38efe7adececb41
kiran1125 <makamkiran95@gmail.com>
2021-11-05 07:14:38 +0530

    Added : Parameterised Constructor and class level Generic method

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -9,25 +9,32 @@ public class MaximumTest<T extends Comparable> {
    //class level variables
    public T x,y,z;
 
-
    /**
-    *passing three objects as parameters0 to get maximum number
+    * parameterised constructor to assign values to the instance variables
     * @param x
     * @param y
     * @param z
-    * @return maximum
     */
-   T maximumOfObjects(T x,T y, T z){
+   MaximumTest(T x,T y,T z){
       this.x = x;
       this.y = y;
       this.z = z;
+   }
+
+
+   /**
+    *passing three objects as parameters to get maximum number
+    * @return maximum
+    */
+   T maximumOfObjects(T x,T y,T z){
       T maximum = null;
       if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
          maximum = x;
-      if (y.compareTo(x) > 0 && y.compareTo(z) > 0)
+      else if (y.compareTo(x) > 0 && y.compareTo(z) > 0)
          maximum = y;
-      if (z.compareTo(x) > 0 && z.compareTo(y) > 0)
+      else
          maximum = z;
-      return maximum;
+      return maximum;   //returning largest one
    }
+
 }
