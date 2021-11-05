b339fd034b63a2d54d84a00fd8c12febe12b764a
kiran1125 <makamkiran95@gmail.com>
2021-11-05 07:45:30 +0530

    Added : Finding largest one in n elements as parameters

--- src/com/bridgelabz/MaximumTest.java
+++ src/com/bridgelabz/MaximumTest.java
@@ -5,36 +5,19 @@
  */
 package com.bridgelabz;
 
-public class MaximumTest<T extends Comparable> {
-   //class level variables
-   public T x,y,z;
-
+public class MaximumTest<T extends Comparable<T>> {
    /**
-    * parameterised constructor to assign values to the instance variables
-    * @param x
-    * @param y
-    * @param z
-    */
-   MaximumTest(T x,T y,T z){
-      this.x = x;
-      this.y = y;
-      this.z = z;
-   }
-
-
-   /**
-    *passing three objects as parameters to get maximum number
+    * Finding maximum for the n elements
+    * @param element
     * @return maximum
     */
-   T maximumOfObjects(T x,T y,T z){
-      T maximum = null;
-      if(x.compareTo(y) > 0 && x.compareTo(z) > 0)
-         maximum = x;
-      else if (y.compareTo(x) > 0 && y.compareTo(z) > 0)
-         maximum = y;
-      else
-         maximum = z;
-      return maximum;   //returning largest one
-   }
 
+   T maximum(T ...element ){
+      T maximum = element[0];
+      for (T a:element) {
+         if(a.compareTo(maximum) > 0)
+            maximum = a;
+      }
+      return maximum;
+   }
 }
--- src/com/bridgelabz/PrintMaximumValue.java
+++ src/com/bridgelabz/PrintMaximumValue.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 public class PrintMaximumValue {
     public static void main(String[] args) {
-
+        MaximumTest maximumTest = new MaximumTest();
+        System.out.println(maximumTest.maximum(2,3,4,1,4,25,67));
     }
 }
